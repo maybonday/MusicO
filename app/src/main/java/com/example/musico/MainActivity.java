2c34dfc4d83e478814ac8a78bc6d4c28b8058aee
Ömer Musab Çiçek <omusab@marun.edu.tr>
2026-01-03 18:55:37 +0300

    Namespace hatası giderildi ve Java altyapısı kuruldu

--- app/src/main/java/com/example/musico/MainActivity.java
+++ app/src/main/java/com/example/musico/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.musico;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                // Ana sayfaya tıklandığında ne olsun?
+                Toast.makeText(this, "Ana Sayfa", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.nav_search) {
+                // Arama kısmına tıklandığında ne olsun?
+                Toast.makeText(this, "Arama Ekranı", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (id == R.id.nav_library) {
+                // Kitaplığa tıklandığında ne olsun?
+                Toast.makeText(this, "Kitaplığın", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
